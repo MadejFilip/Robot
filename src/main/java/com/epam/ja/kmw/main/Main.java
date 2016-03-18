1666ac25f2c67774cd68d20df3d21d4db3769b4b
Marcin <marcin_bulawa@epam.com>
2016-03-18 12:45:31 +0100

    Log4j Added

--- src/main/java/com/epam/ja/kmw/main/Main.java
+++ src/main/java/com/epam/ja/kmw/main/Main.java
@@ -1,8 +1,17 @@
 package com.epam.ja.kmw.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
-	public static void main(String[] args) {
+	private static final Logger logger = LogManager.getLogger(Main.class);
+
+	public static void main(final String... args) {
+
+		// Set up a simple configuration that logs on the console.
+
+		logger.trace("Starting our great robot application.");
 
 	}
 }
