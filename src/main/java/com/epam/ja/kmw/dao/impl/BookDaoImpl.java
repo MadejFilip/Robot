725aa7dcd7b6f3d0fee1212e4b2b3d438b93b16b
BulaMar <bulawa_marcin@wp.pl>
2016-03-20 10:34:10 +0100

    Dao Implementation package added with BookDaoImpl class  which implements BookDao interface

---
+++ src/main/java/com/epam/ja/kmw/dao/impl/BookDaoImpl.java
@@ -0,0 +1,35 @@
+package com.epam.ja.kmw.dao.impl;
+
+import java.util.List;
+
+import com.epam.ja.kmw.dao.BookDao;
+import com.epam.ja.kmw.model.Book;
+
+public class BookDaoImpl implements BookDao {
+
+	public void addBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void delBook(int bookId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Book getBookById(int bookId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
