cae38462ea5a3c8b5961b21bacef2a1834abba20
Kacper Jackiewicz <Kacper_Jackiewicz@epam.com>
2016-03-21 11:15:29 +0100

    not finished librarychecker, commit for Marcin

--- src/main/java/com/epam/ja/kmw/main/Main.java
+++ src/main/java/com/epam/ja/kmw/main/Main.java
@@ -1,5 +1,8 @@
 package com.epam.ja.kmw.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,19 +22,19 @@ public class Main {
 		// Set up a simple configuration that logs on the console.
 
 		LOGGER.trace("Starting our great robot application.");
-		BookStore lib = new BookStore("library", "http://www.nexto.pl/ebooki/darmowe_c1219.xml?_offset=10",
+		BookStore lib = new BookStore("www.nexto.pl", "http://www.nexto.pl/ebooki_c1015.xml?_order=-5#filters",
 				"<ul class=\"productslist\">", "<li>", "<h3>", "<strong class=\"nprice\">");
 		
-		Book[] b = null;
+		List<Book> bookList = new ArrayList<>();
 		try {
-			b = LibraryChecker.getFreeBooks(lib);
+			bookList.addAll(LibraryChecker.getFreeBooks(lib));
 		} catch (NotFound | ResponseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		
-		for(Book bk : b) {
+		for(Book bk : bookList) {
 			System.out.println(bk);
 		}
 		
--- src/main/java/scraping/LibraryChecker.java
+++ src/main/java/scraping/LibraryChecker.java
@@ -1,10 +1,12 @@
 package scraping;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.epam.ja.kmw.model.Book;
 import com.epam.ja.kmw.model.BookStore;
+import com.jaunt.Document;
 import com.jaunt.Element;
 import com.jaunt.Elements;
 import com.jaunt.NotFound;
@@ -14,20 +16,38 @@ import com.jaunt.UserAgent;
 public class LibraryChecker {
 	private static final UserAgent userAgent = new UserAgent();
 	
-	public static Book[] getFreeBooks(BookStore library) throws ResponseException, NotFound{
+	public static List<Book> getFreeBooks(BookStore library) throws ResponseException, NotFound{
 		List<Book> bookList = new ArrayList<>();
-		
-		userAgent.visit(library.getUrl());
+		String url = library.getUrl(); 
+		boolean flag = false;
+		while(!flag) {		
+		try {
+			userAgent.doc=userAgent.visit(url);
+			bookList.addAll(getFromSubSite(library));
+			url=userAgent.doc.findFirst("<a class=\"next\">").getAt("href").toString();
+			System.out.println(url);
+			
+		} catch(NotFound | ResponseException nf){
+			nf.printStackTrace();
+			flag=true;
+		}
+		}
+	
+				
+		return bookList;
+	}
+	
+	private static List<Book> getFromSubSite(BookStore library) throws NotFound, ResponseException {
+
+		List<Book> subsiteBookList = new ArrayList<>();
 		Elements links = userAgent.doc.findFirst(library.getTag()).findEach(library.getContainer());
 		for(Element element : links)
 		{		
 			
 			if(element.findFirst(library.getPriceTag()).innerText().contains("0,00"))
-			bookList.add(new Book(element.getFirst(library.getNameTag()).innerText(),library.getName()));
-			
+			subsiteBookList.add(new Book(element.getFirst(library.getNameTag()).innerText(),library.getName()));
+			System.out.println(element.getFirst(library.getNameTag()).innerText());
 		}
-	
-				
-		return bookList.toArray(new Book[bookList.size()]);
+		return subsiteBookList;
 	}
 }
