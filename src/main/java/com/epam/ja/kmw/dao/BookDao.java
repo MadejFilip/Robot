f891c8c5ec9b5144a38c6f6174b8e1fbc0129b2d
Marcin <marcin_bulawa@epam.com>
2016-03-22 12:49:06 +0100

    Some modyfication in DAO of Book

--- src/main/java/com/epam/ja/kmw/dao/BookDao.java
+++ src/main/java/com/epam/ja/kmw/dao/BookDao.java
@@ -7,12 +7,17 @@ import com.epam.ja.kmw.model.Book;
 public interface BookDao {
 
 	public boolean addBook(Book book);
-	
-	public boolean updateBook (Book book);
-	
+
+	public boolean addAllBooks(List<Book> listOfBooks);
+
+	public boolean updateBook(Book book);
+
 	public boolean delBook(int bookId);
-	
+
 	public List<Book> getAllBooks();
 	
+	public List<Book> getAllBooksForOneBookStore(String bookStoreName);
+
 	public Book getBookById(int bookId);
+
 }
